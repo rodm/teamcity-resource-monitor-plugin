7f474123f0183f985d989aca044a4675a15f0e73
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2011-09-25 12:50:48 +0100

    Implemented methods to allow a build configuration to be linked and unlinked with a resource.

--- src/main/java/teamcity/resource/ResourceMonitor.java
+++ src/main/java/teamcity/resource/ResourceMonitor.java
@@ -44,6 +44,10 @@ public class ResourceMonitor implements Runnable {
         this.projectManager = projectManager;
     }
 
+    public void setProjectMananger(ProjectManager newProjectMananger) {
+        this.projectManager = newProjectMananger;
+    }
+
     public void setInterval(int interval) {
         this.interval = interval;
     }
@@ -130,9 +134,7 @@ public class ResourceMonitor implements Runnable {
     }
 
     public void removeResource(String name) {
-        if (!resources.containsKey(name)) {
-            throw new IllegalArgumentException("resource with name " + name + " does not exist");
-        }
+        validResource(name);
         resources.remove(name);
     }
 
@@ -142,5 +144,32 @@ public class ResourceMonitor implements Runnable {
 
     public Map<String,Resource> getResources() {
         return resources;
+    }
+
+    public void linkBuildToResource(String name, String buildTypeId) {
+        validResource(name);
+        validBuildType(buildTypeId);
+        Resource resource = resources.get(name);
+        resource.getBuildTypes().add(buildTypeId);
+    }
+
+    public void unlinkBuildFromResource(String name, String buildTypeId) {
+        validResource(name);
+        validBuildType(buildTypeId);
+        Resource resource = resources.get(name);
+        resource.getBuildTypes().remove(buildTypeId);
+    }
+
+    private void validResource(String name) {
+        if (!resources.containsKey(name)) {
+            throw new IllegalArgumentException("resource with name " + name + " does not exist");
+        }
+    }
+
+    private void validBuildType(String buildTypeId) {
+        SBuildType buildType = projectManager.findBuildTypeById(buildTypeId);
+        if (buildType == null) {
+            throw new IllegalArgumentException("build type id " + buildTypeId + " does not exist");
+        }
     }
 }
--- src/test/java/teamcity/resource/ResourceMonitorTest.java
+++ src/test/java/teamcity/resource/ResourceMonitorTest.java
@@ -1,8 +1,15 @@
 package teamcity.resource;
 
+import jetbrains.buildServer.serverSide.ProjectManager;
+import jetbrains.buildServer.serverSide.SBuildType;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static junit.framework.Assert.assertEquals;
@@ -40,5 +47,97 @@ public class ResourceMonitorTest {
     public void cannotAddResourceWithSameName() {
         monitor.addResource(new Resource("Test Resource", null, -1));
         monitor.addResource(new Resource("Test Resource", null, -1));
+    }
+
+    @Test
+    public void removeResource() {
+        monitor.addResource(new Resource("Test Resource", null, -1));
+
+        monitor.removeResource("Test Resource");
+        assertEquals("there should be no resources", 0, monitor.getResources().size());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void removingResourceThatDoesntExist() {
+        monitor.removeResource("Resource");
+    }
+
+    @Test
+    public void linkBuildToResource() {
+        monitor.setProjectMananger(createProjectManager());
+        monitor.addResource(new Resource("Test Resource", null, -1));
+
+        monitor.linkBuildToResource("Test Resource", "bt123");
+
+        Map<String, Resource> resources = monitor.getResources();
+        assertEquals(1, resources.get("Test Resource").getBuildTypes().size());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void linkBuildToInvalidResource() {
+        monitor.linkBuildToResource("Test Resource", "bt123");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void linkInvalidBuildToResource() {
+        monitor.setProjectMananger(createProjectManager());
+        monitor.addResource(new Resource("Test Resource", null, -1));
+
+        monitor.linkBuildToResource("Test Resource", "bt124");
+    }
+
+    @Test
+    public void unlinkBuildFromResource() {
+        monitor.setProjectMananger(createProjectManager());
+        List<String> buildTypes = new ArrayList<String>();
+        buildTypes.add("bt123");
+        Resource resource = new Resource("Test Resource", null, -1);
+        resource.setBuildTypes(buildTypes);
+        monitor.addResource(resource);
+
+        monitor.unlinkBuildFromResource("Test Resource", "bt123");
+
+        Map<String, Resource> resources = monitor.getResources();
+        assertEquals(0, resources.get("Test Resource").getBuildTypes().size());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void unlinkBuildFromInvalidResource() {
+        monitor.unlinkBuildFromResource("Test Resource", "bt123");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void unlinkInvalidBuildFromResource() {
+        monitor.setProjectMananger(createProjectManager());
+        monitor.addResource(new Resource("Test Resource", null, -1));
+
+        monitor.unlinkBuildFromResource("Test Resource", "bt124");
+    }
+
+    private ProjectManager createProjectManager() {
+        ClassLoader loader = ProjectManager.class.getClassLoader();
+        Class[] proxyInterfaces = new Class[] { ProjectManager.class };
+        InvocationHandler handler = new InvocationHandler() {
+            public Object invoke(Object obj, Method method, Object[] objects) throws Throwable {
+                if ("findBuildTypeById".equals(method.getName())) {
+                    if ("bt123".equals(objects[0])) {
+                        return createBuildType();
+                    }
+                }
+                return null;
+            }
+
+            private SBuildType createBuildType() {
+                ClassLoader loader = SBuildType.class.getClassLoader();
+                Class[] proxyInterfaces = new Class[] { SBuildType.class };
+                InvocationHandler handler = new InvocationHandler() {
+                    public Object invoke(Object obj, Method method, Object[] objects) throws Throwable {
+                        return null;
+                    }
+                };
+                return (SBuildType) Proxy.newProxyInstance(loader, proxyInterfaces, handler);
+            }
+        };
+        return (ProjectManager) Proxy.newProxyInstance(loader, proxyInterfaces, handler);
     }
 }
