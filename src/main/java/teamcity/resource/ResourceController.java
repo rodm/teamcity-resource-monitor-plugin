511a1bee3251609019fa3847cb31c20396abdec8
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2012-07-05 23:13:03 +0100

    Replaced duplicate strings with constants.

--- src/main/java/teamcity/resource/ResourceController.java
+++ src/main/java/teamcity/resource/ResourceController.java
@@ -15,6 +15,21 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ResourceController extends BaseController {
 
+    private static final String ACTION_PARAMETER = "submitAction";
+    private static final String ID_PARAMETER = "resourceId";
+    private static final String NAME_PARAMETER = "resourceName";
+    private static final String HOST_PARAMETER = "resourceHost";
+    private static final String PORT_PARAMETER = "resourcePort";
+    private static final String BUILD_TYPE_ID_PARAMETER = "buildTypeId";
+
+    private static final String ADD_ACTION = "addResource";
+    private static final String UPDATE_ACTION = "updateResource";
+    private static final String REMOVE_ACTION = "removeResource";
+    private static final String LINK_BUILD_ACTION = "linkBuildType";
+    private static final String UNLINK_BUILD_ACTION = "unlinkBuildType";
+    private static final String ENABLE_ACTION = "enableResource";
+    private static final String DISABLE_ACTION = "disableResource";
+
     private final WebControllerManager webControllerManager;
 
     private ResourceManager resourceManager;
@@ -62,46 +77,46 @@ public class ResourceController extends BaseController {
 
     private void doAction(final HttpServletRequest request) throws Exception {
         Loggers.SERVER.debug("       method: [" + request.getMethod() + "]");
-        Loggers.SERVER.debug("submit action: [" + request.getParameter("submitAction") + "]");
-        Loggers.SERVER.debug("  resource id: [" + request.getParameter("resourceId") + "]");
-        Loggers.SERVER.debug("resource name: [" + request.getParameter("resourceName") + "]");
-        Loggers.SERVER.debug("resource host: [" + request.getParameter("resourceHost") + "]");
-        Loggers.SERVER.debug("resource port: [" + request.getParameter("resourcePort") + "]");
-        Loggers.SERVER.debug("build type id: [" + request.getParameter("buildTypeId") + "]");
+        Loggers.SERVER.debug("submit action: [" + request.getParameter(ACTION_PARAMETER) + "]");
+        Loggers.SERVER.debug("  resource id: [" + request.getParameter(ID_PARAMETER) + "]");
+        Loggers.SERVER.debug("resource name: [" + request.getParameter(NAME_PARAMETER) + "]");
+        Loggers.SERVER.debug("resource host: [" + request.getParameter(HOST_PARAMETER) + "]");
+        Loggers.SERVER.debug("resource port: [" + request.getParameter(PORT_PARAMETER) + "]");
+        Loggers.SERVER.debug("build type id: [" + request.getParameter(BUILD_TYPE_ID_PARAMETER) + "]");
 
-        String action = request.getParameter("submitAction");
-        if ("addResource".equals(action)) {
-            String name = request.getParameter("resourceName");
-            String host = request.getParameter("resourceHost");
-            String port = request.getParameter("resourcePort");
+        String action = request.getParameter(ACTION_PARAMETER);
+        if (ADD_ACTION.equals(action)) {
+            String name = request.getParameter(NAME_PARAMETER);
+            String host = request.getParameter(HOST_PARAMETER);
+            String port = request.getParameter(PORT_PARAMETER);
             resourceManager.addResource(name, host, port);
             plugin.saveConfiguration();
-        } else if ("updateResource".equals(action)) {
-            String id = request.getParameter("resourceId");
-            String name = request.getParameter("resourceName");
-            String host = request.getParameter("resourceHost");
-            String port = request.getParameter("resourcePort");
+        } else if (UPDATE_ACTION.equals(action)) {
+            String id = request.getParameter(ID_PARAMETER);
+            String name = request.getParameter(NAME_PARAMETER);
+            String host = request.getParameter(HOST_PARAMETER);
+            String port = request.getParameter(PORT_PARAMETER);
             resourceManager.updateResource(id, name, host, port);
             plugin.saveConfiguration();
-        } else if ("removeResource".equals(action)) {
-            String id = request.getParameter("resourceId");
+        } else if (REMOVE_ACTION.equals(action)) {
+            String id = request.getParameter(ID_PARAMETER);
             resourceManager.removeResource(id);
             plugin.saveConfiguration();
-        } else if ("linkBuildType".equals(action)) {
-            String id = request.getParameter("resourceId");
-            String buildTypeId = request.getParameter("buildTypeId");
+        } else if (LINK_BUILD_ACTION.equals(action)) {
+            String id = request.getParameter(ID_PARAMETER);
+            String buildTypeId = request.getParameter(BUILD_TYPE_ID_PARAMETER);
             resourceManager.linkBuildToResource(id, buildTypeId);
             plugin.saveConfiguration();
-        } else if ("unlinkBuildType".equals(action)) {
-            String id = request.getParameter("resourceId");
-            String buildTypeId = request.getParameter("buildTypeId");
+        } else if (UNLINK_BUILD_ACTION.equals(action)) {
+            String id = request.getParameter(ID_PARAMETER);
+            String buildTypeId = request.getParameter(BUILD_TYPE_ID_PARAMETER);
             resourceManager.unlinkBuildFromResource(id, buildTypeId);
             plugin.saveConfiguration();
-        } else if ("enableResource".equals(action)) {
-            String id = request.getParameter("resourceId");
+        } else if (ENABLE_ACTION.equals(action)) {
+            String id = request.getParameter(ID_PARAMETER);
             monitor.enableResource(resourceManager.getResourceById(id));
-        } else if ("disableResource".equals(action)) {
-            String id = request.getParameter("resourceId");
+        } else if (DISABLE_ACTION.equals(action)) {
+            String id = request.getParameter(ID_PARAMETER);
             monitor.disableResource(resourceManager.getResourceById(id));
         } else {
             throw new IllegalArgumentException("Invalid action: " + action);
